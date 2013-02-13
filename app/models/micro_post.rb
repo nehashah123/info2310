9a29662c433d8a40663e1104d53b5c6c8c3cd5ad
nehashah123 <nss57@cornell.edu>
2013-02-13 19:45:01 +0000

    user model still got issues

--- app/models/micro_post.rb
+++ app/models/micro_post.rb
@@ -1,5 +1,6 @@
 class MicroPost < ActiveRecord::Base
   attr_accessible :content, :user_id
+  belongs_to :user
   validates :user_id, presence: true
   validates :content, presence: true,
                       length: { minimum: 5, maximum: 140 }
